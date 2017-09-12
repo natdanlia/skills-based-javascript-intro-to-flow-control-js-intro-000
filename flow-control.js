4ad1b939cf206386d065e75eef1a935243917be1
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-09-12 20:44:11 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,9 @@
 function basicTeenager(age) {
   switch (age) {
     case age >= 13 && age <= 19:
-      return "You are a teenager!";
+      return "You are a teenager!"
     default:
-      return "You have an age";
+      return "You have an age"
 
   }
 }
